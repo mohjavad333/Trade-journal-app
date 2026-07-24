c1b22be2786c934fa8faf65082c058dcb2b48ace
Mohammad javad Rezaei <mohammadjavadrezaei333@gmail.com>
2026-07-24 17:32:01 +0330

    edit app.tsx file

--- client/App.tsx
+++ client/App.tsx
@@ -8,11 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Index from "./pages/Index";
-import Dashboard from "./pages/Dashboard";
-import Setups from "./pages/Setups";
-import Journal from "./pages/Journal";
-import Tools from "./pages/Tools";
-import Settings from "./pages/Settings";
+
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -26,11 +22,6 @@ const App = () => (
         <Header />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/setups" element={<Setups />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/settings" element={<Settings />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
